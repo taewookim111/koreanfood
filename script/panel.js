81f2ab82e22447f68c76c26f4ceb9f2f5086e60f
taewookim111 <taewookim000p@gmail.com>
2023-09-01 15:15:30 +0900

    한식포트폴리오수정

--- script/panel.js
+++ script/panel.js
@@ -1,12 +1,12 @@
 const panel = document.querySelector(".panel");
 const panel_li = panel.querySelectorAll("li");
-const btns = document.querySelectorAll(".btns li");
+const btnPanel = document.querySelectorAll(".btns li");
 
 
-btns.forEach((el, index)=>{
+btnPanel.forEach((el, index)=>{
     el.addEventListener("click", (e)=>{
         e.preventDefault();
-        add(btns, index);
+        add(btnPanel, index);
         play(panel_li, index);
         panel.style.marginLeft = `${-100*index}%`;
     })
---
+++ script/tapmenu.js
@@ -0,0 +1,4 @@
+const box = document.querySelector("#box");
+const btnTap = document.querySelectorAll("ul li");
+const articles = document.querySelectorAll("section article");
+
